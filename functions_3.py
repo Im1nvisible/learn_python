419fe2442b8fce518e478bbbea64e51576e14090
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-05-23 13:02:11 +0300

    Create functions_3.py

---
+++ functions_3.py
@@ -0,0 +1,14 @@
+def get_sum(a, b):
+    '''
+    return summ elements
+
+    :param a: first operand
+    :type a: int
+    :param b: second operand
+    :type b: int
+    :return: return type int
+    '''
+    return a + b
+
+print(get_sum(1, 2))
+
