5b8935ce1765ca4669f80677b41c91fa976588eb
vitaliy_somoniuk <vitaliysomonyuk@gmail.com>
2020-05-23 13:23:56 +0300

    Update functions_3.py
    add code in file

--- functions_3.py
+++ functions_3.py
@@ -10,5 +10,32 @@ def get_sum(a, b):
     '''
     return a + b
 
-print(get_sum(1, 2))
+#print(get_sum(1, 2))
 
+a = 5 #global
+
+def f():
+    global a
+    a += 1
+
+print(a)
+f()
+print(a)
+
+
+num = [1, '2', 3]
+#1
+def l(num):
+    return [i * 2 for i in num]
+
+print(l(num))
+
+
+#2
+def l2(num):
+    def get_mult(x):
+        if isinstance(x, int):
+            return x * 2
+    return [get_mult(i) for i in num if get_mult(i)]
+
+print(l2(num))
